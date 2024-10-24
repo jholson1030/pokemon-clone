004fedf01ca0c38a6e2112e008ee42309248e7e0
jholson1030 <jholson1030@gmail.com>
2024-10-24 18:32:00 -0500

    imported previous work

---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = './img/Pellet Town.png';
+
+const playerImage = new Image();
+playerImage.src = './img/playerDown.png';
+
+
+image.onload = () => {
+    c.drawImage(image, -760, -585);
+
+}
+
+
+playerImage.onload = () => {
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height, 
+        canvas.width / 2 - playerImage.width / 4, 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    );
+}
